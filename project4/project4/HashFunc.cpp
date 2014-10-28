94d08e9d1ed521d62d7a8f74443dcf594627d1c9
NATE <seifertn@msu.edu>
2014-10-28 13:04:39 -0400

    Started repo

---
+++ project4/project4/HashFunc.cpp
@@ -0,0 +1,30 @@
+#include "HashFunc.h"
+
+using namespace std;
+
+/**
+ * Put information about this hash function here
+ */
+unsigned int HashA(const std::string& s)
+{
+	// TODO
+	return 0;
+}
+
+/**
+ * Put information about this hash function here
+ */
+unsigned int HashB(const std::string& s)
+{
+	// TODO
+	return 0;
+}
+
+/**
+ * Put information about this hash function here
+ */
+unsigned int HashC(const std::string& s)
+{
+	// TODO
+	return 0;
+}
